183f3bc914e8fc6ba0e8d6a324fe486ae78be918
andrewchung123 <41243131@nfu.edu.tw>
2024-11-18 20:55:19 +0800

    Update tcpfilesender.h

--- tcpfilesender.h
+++ tcpfilesender.h
@@ -10,29 +10,32 @@ class TcpFileSender : public QDialog
     Q_OBJECT
 
 public:
-    TcpFileSender(QWidget *parent = 0);
+    TcpFileSender(QWidget *parent = nullptr);
     ~TcpFileSender();
-public slots:
-    void start();
-    void startTransfer();
-    void updateClientProgress(qint64 numBytes);
-    void openFile();
-private:
-    QProgressBar     *clientProgressBar;
-    QLabel           *clientStatusLabel;
-    QPushButton      *startButton;
-    QPushButton      *quitButton;
-    QPushButton      *openButton;
-    QDialogButtonBox *buttonBox;
-    QTcpSocket       tcpClient;
 
-    qint64           totalBytes;
-    qint64           bytesWritten;
-    qint64           bytesToWrite;
-    qint64           loadSize;
-    QString          fileName;
-    QFile            *localFile;
-    QByteArray       outBlock;
+private slots:
+    void openFile();               // 開檔方法
+    void startTransfer();          // 開始傳送方法
+    void updateClientProgress(qint64 numBytes); // 更新進度條
+    void enableStartButton();      // 啟用開始按鈕的方法
+    void start();                  // 開始傳送檔案
+
+private:
+    QLineEdit *ipLineEdit;
+    QLineEdit *portLineEdit;
+    QProgressBar *clientProgressBar;
+    QLabel *clientStatusLabel;
+    QPushButton *startButton;
+    QPushButton *quitButton;
+    QPushButton *openButton;
+    QTcpSocket tcpClient;
+    QFile *localFile;
+    QString fileName;
+    QByteArray outBlock;
+    qint64 totalBytes;
+    qint64 bytesWritten;
+    qint64 bytesToWrite;
+    int loadSize;
 };
 
 #endif // TCPFILESENDER_H
